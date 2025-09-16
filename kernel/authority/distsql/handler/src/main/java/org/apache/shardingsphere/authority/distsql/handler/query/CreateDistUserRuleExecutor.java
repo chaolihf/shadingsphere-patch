d2d1daaecf102333c2bb094039f22b00f718604f
chaoli <lichao01.ah@chinatelecom.cn>
2025-09-16 22:34:01 +0800

    standalone模式支持postgresql数据库

--- kernel/authority/distsql/handler/src/main/java/org/apache/shardingsphere/authority/distsql/handler/query/CreateDistUserRuleExecutor.java
+++ kernel/authority/distsql/handler/src/main/java/org/apache/shardingsphere/authority/distsql/handler/query/CreateDistUserRuleExecutor.java
@@ -18,7 +18,6 @@
 package org.apache.shardingsphere.authority.distsql.handler.query;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.apache.shardingsphere.authority.config.AuthorityRuleConfiguration;
